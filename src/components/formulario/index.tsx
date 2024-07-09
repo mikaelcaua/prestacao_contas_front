5577ca3ccce4355c67bd2b436deba57cce7258c5
Mikael Cauã <mikaelcauasilva1@gmail.com>
2024-07-09 16:45:06 -0300

    add form fields

--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -1,18 +1,134 @@
 import { useState } from "react"
 import postPrestacoes from "../../service/postPrestacoes"
-
+import IPrestacao from "../../types/IPrestacao"
 export default function Formulario(){
-    let [descricao,setDescricao] = useState<string>('')
+    let [dadosForm, setDadosForm] = useState<IPrestacao>({
+        descricao: '',
+        instituicao: '',
+        cpf: '',
+        tipoPagamento: '',
+        dataInicial: '',
+        formaPagamento: '',
+        dataFinal: '',
+        valor: '',
+        mes: '',
+        ano: '',
+    });
     return(
         <form onSubmit={e => {
             e.preventDefault()
-            postPrestacoes(descricao)
+            postPrestacoes(dadosForm)
         }}>
             <input
                 type="text"
-                onChange={e => setDescricao(e.target.value)}
+                id="descricao"
+                value={dadosForm.descricao}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        descricao: e.target.value
+                    }))
+                }
             />
-
+            <input
+                type="text"
+                id="instituicao"
+                value={dadosForm.instituicao}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        instituicao: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="cpf"
+                value={dadosForm.cpf}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        cpf: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="tipoPagamento"
+                value={dadosForm.tipoPagamento}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        tipoPagamento: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="dataInicial"
+                value={dadosForm.dataInicial}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        dataInicial: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="formaPagamento"
+                value={dadosForm.formaPagamento}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        formaPagamento: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="dataFinal"
+                value={dadosForm.dataFinal}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        dataFinal: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="valor"
+                value={dadosForm.valor}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        valor: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="mes"
+                value={dadosForm.mes}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        mes: e.target.value
+                    }))
+                }
+            />
+            <input
+                type="text"
+                id="ano"
+                value={dadosForm.ano}
+                onChange={(e) =>
+                    setDadosForm((prevDadosForm) => ({
+                        ...prevDadosForm,
+                        ano: e.target.value
+                    }))
+                }
+            />
             <button type="submit">Botao</button>
         </form>
     )
--- src/service/postPrestacoes.ts
+++ src/service/postPrestacoes.ts
@@ -1,12 +1,35 @@
-// service/postPrestacoes.ts
-export default async function postPrestacoes(descricao: string) {
+import IPrestacao from '../types/IPrestacao'
+export default async function postPrestacoes({
+    descricao,
+    instituicao,
+    cpf,
+    tipoPagamento,
+    dataInicial,
+    formaPagamento,
+    dataFinal,
+    valor,
+    mes,
+    ano,
+    id
+}: IPrestacao) {
   try {
       const response = await fetch('http://127.0.0.1:5000', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ descricao }),
+          body: JSON.stringify({
+            descricao:descricao,
+            instituicao:instituicao,
+            cpf:cpf,
+            tipoPagamento:tipoPagamento,
+            dataInicial:dataInicial,
+            formaPagamento:formaPagamento,
+            dataFinal:dataFinal,
+            valor:valor,
+            mes:mes,
+            ano:ano
+          }),
       });
 
       if (!response.ok) {
